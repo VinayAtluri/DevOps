975a9a872def557f5dd2558aa960f4e91ad3aee8
Vinay <Vinay@LAPTOP-6E0U31BJ>
2021-02-24 16:15:33 +0530

    Second commit to the Git repository

--- DecOps/src/Sample1.java
+++ DecOps/src/Sample1.java
@@ -3,5 +3,6 @@ public class Sample1 {
 
 	public static void main(String[] args) {
 		System.out.println("First sample line");
+		System.out.println("Created new repository and pushed code");
 	}
 }
